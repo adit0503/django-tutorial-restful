e1fe5d41f84c51dd89e903c3bc1e7307e182e516
adit0503 <adit0503@gmail.com>
2020-08-22 10:19:21 +0530

    trying git from atom

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'snippets.apps.SnippetsConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -20,11 +20,13 @@ from rest_framework import routers
 from quickstart import views
 
 router  = routers.DefaultRouter()
+
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('snippets/', include('snippets.urls')),
+    path('quickstart/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
